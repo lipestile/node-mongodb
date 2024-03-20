cf1dec52fc07a81af7a4d00bbabb0504e31402fa
Lipestile <felipebalbino53gmail.com>
2024-03-20 20:35:48 -0300

    anonimato

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function main() {
   try {
     await mongoose.connect(
-      "mongodb+srv://felipebalbino53:yv775FDN2DIki0qS@cluster0.ri3p7g6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      `mongodb+srv://${process.env.DB_MONG_USER}:${process.env.DB_MONG_PWD}@cluster0.ri3p7g6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"`
     );
     console.log("Banco OK");
   } catch (error) {
